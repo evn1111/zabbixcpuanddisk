dbef16ffdb137b45c83014b76faa3f4033ea5962
evn1111 <138611344+evn1111@users.noreply.github.com>
2023-10-12 21:35:42 +0300

    Update cpu_temp.sh

--- cpu_temp.sh
+++ cpu_temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Используем команду sensors для получения температуры CPU
-cpu_temp=$(sensors | grep "CPU:" | awk '{print $2}' | sed 's/+//')
+cpu_temp=$(sensors | grep "CPU:" | awk '{print $2}' | sed 's/+//;s/°C//')
 
 # Выводим температуру в формате, понимаемом Zabbix
 echo "$cpu_temp"
